e98c49bdbb15c3c93157937cbb929d79cf86cf02
Adnan Crnovrsanin <adnancrnovrsanin48@gmail.com>
2024-04-12 09:56:46 +0200

    Fixing bugs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function myTopics()
     {
-        $topics = auth()->user()->topics;
+        if (!auth()->user()) {
+            return redirect()->route('login');
+        }
+
+        $user = User::find(auth()->user()->getAuthIdentifier());
+
+        $topics = $user->topics;
 
         return view('user.topics', ['topics' => $topics]);
     }
 
     public function myConversations()
     {
-        $conversations = auth()->user()->conversations;
+        if (!auth()->user()) {
+            return redirect()->route('login');
+        }
+
+        $user = User::find(auth()->user()->getAuthIdentifier());
+
+        $conversations = $user->conversations;
 
         return view('user.conversations', ['conversations' => $conversations]);
     }
